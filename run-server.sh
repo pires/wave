d5f5aa8c07fa2d5ec29a4bf007765d816d751724
Paulo Pires <pjpires@gmail.com>
2012-05-29 23:07:56 +0100

    Added needed files for running WIAB out-of-the-box.
    Signed-off-by: Paulo Pires <pjpires@gmail.com>

---
+++ run-server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script will start the Wave in a Box server.
+#
+
+# Make sure the config file exists.
+if [ ! -e server.config ]; then
+  echo "You need to copy server.config.example to server.config and edit it. Or run: 'ant -f server-config.xml' to generate the file automatically."
+  exit 1
+fi
+
+. process-script-args.sh
+
+exec java $DEBUG_FLAGS \
+  -Dorg.eclipse.jetty.util.log.DEBUG=true \
+  -Djava.security.auth.login.config=jaas.config \
+  -Dwave.server.config=server.config \
+  -jar box/target/box-0.3-SNAPSHOT.jar
