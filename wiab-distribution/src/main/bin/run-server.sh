83b411921722f3ec900bb331c9dc09de030076a1
Michael MacFadden <michael@macfadden.org>
2012-06-09 07:35:04 -0700

    A fix for issue 9, creating a multi-jar build.

--- wiab-distribution/src/main/bin/run-server.sh
+++ wiab-distribution/src/main/bin/run-server.sh
@@ -16,3 +16,14 @@ exec java $DEBUG_FLAGS \
   -Djava.security.auth.login.config=jaas.config \
   -Dwave.server.config=server.config \
   -jar lib/box-0.3-SNAPSHOT.jar
+  
+
+# Alternate run configuration which doesn't use the executable jar.
+# Compute the classpath based on all of the jars in the lib dir.
+# CLASSPATH=`echo lib/*.jar | tr ' ' ':'`
+# exec java $DEBUG_FLAGS \
+#  -Dorg.eclipse.jetty.LEVEL=DEBUG \
+#  -Djava.security.auth.login.config=jaas.config \
+#  -Dwave.server.config=server.config \
+#  -cp $CLASSPATH \ 
+#  org.waveprotocol.box.server.ServerMain 
